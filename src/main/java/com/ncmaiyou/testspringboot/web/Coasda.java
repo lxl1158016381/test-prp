ea4c87fb69456654755e1c57d93067b1dde0f746
luoxinlin <1158016381@qq.com>
2021-09-05 18:25:43 +0800

    sad

---
+++ src/main/java/com/ncmaiyou/testspringboot/web/Coasda.java
@@ -0,0 +1,12 @@
+package com.ncmaiyou.testspringboot.web;
+
+/**
+ * @author luoxinlin
+ * @date 2021/9/5 18:25
+ * @description
+ */
+public class Coasda {
+    public void test01() {
+        System.out.println("sdasd");
+    }
+}
